f854c3b45c453fdec613dc665863997b6a3b4df6
Nunsen <nuso0001@stud.kea.dk>
2024-04-28 22:35:13 +0200

    Har inddelt vores tidligere priCustomer til en customerInfo som kan genbruges for at undgå redundans... DOG IGEN problemer med at parametre overføre, den vil have at alle parametre skal slettes men det er ikke optimalt. Den printer fortsat i en fil, ved ikke helt om den opretter en ny fil, da vi endnu ikke har lavet to kontrakter i samme run. Formatteret lidt i måden det printes
    OBS PROBLEMER MED AT EFTERNAVN PRINTES VED ADRESSE

--- HonululuCarRental/src/Main.java
+++ HonululuCarRental/src/Main.java
@@ -2,15 +2,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         ourMenu(input);
         pickCustomer(input);
-        writeContract(input);
+        //options(input);
     }
-
     public static void ourMenu(Scanner input) {
         System.out.println("X****************************************************************X");
         System.out.print("\t\tWelcome to Contract Creater\n");
@@ -19,25 +17,26 @@ public class Main {
         System.out.println("X****************************************************************X");
     }
 
-    public static void pickCustomer(Scanner input) {
+    public static void pickCustomer(Scanner input) throws IOException {
         int answer;
 
         System.out.println("Press 1 for Private Renter\nPress 2 for Company Renter");
         answer = input.nextInt();
 
         if (answer == 1) {
-            priCustomer(input);
+            customerInfo(input);
         } else if (answer == 2) {
+            customerInfo(input);
             comCustomer(input);
         }
     }
-    public static void priCustomer(Scanner input) {
+    public static void customerInfo(Scanner input) throws IOException {
         System.out.println("Enter Name of Driver");
         String nameDriver = input.next();
 
         System.out.println("Enter Adress");
-        String address = input.next();
-        input.nextLine(); //Spørg Cay, hvorfor virker det at indsætte nextLine her?
+        String address = input.nextLine();
+        input.nextLine();
 
         System.out.println("Enter zip code");
         int zip = input.nextInt();
@@ -55,46 +54,55 @@ public class Main {
         int licenseNr = input.nextInt();
 
         System.out.println("Enter Driver since date (MM-DD-YY");
+        input.nextLine();
         String sinceDate = input.next();
 
+        writeContract(input, nameDriver, address,zip, city, phoneNr, email, licenseNr, sinceDate);
     }
-        public static void writeContract(Scanner input, String nameDriver, String Address) throws IOException {
-
-            //anvender localdate til at bruge den aktuelle dato på hvornår kontrakten oprettes
-            LocalDate currentDate = LocalDate.now();
-
-            //Vi vil have systemet til at oprette en fil der hedder Contract + dagens dato
-            String fileName = "Contract_" + currentDate + ".txt";
-
-            //Filen oprettes
-            FileWriter writer = new FileWriter(fileName);
-
-            // Write some content to the file
-            writer.write("This is a new contract created on " + currentDate + ".");
-
-            writer.write("Name:" + nameDriver);
-            writer.write("Number:" + address);
-            writer.close();
-        }
     public static void comCustomer(Scanner input) {
-        int answer;
-        String ans;
-
-        System.out.println("Enter Name of Driver");
-        System.out.println("Enter Adress");
-        System.out.println("Enter zip code");
-        System.out.println("Enter city");
-        System.out.println("Enter phone number");
-        System.out.println("Enter e-mail");
-
         System.out.println("Enter Company Name");
+        String comName = input.next();
+
         System.out.println("Enter Company Address");
+        String comAddress = input.next();
+
         System.out.println("Enter Company Phone number");
+        int comPhone = input.nextInt();
+
         System.out.println("Enter Company Registration Number (6 digits)");
+        int comReg = input.nextInt();
 
+        writeContract(input, comName, comAddress, comPhone, comReg);
     }
+    public static void writeContract(Scanner input, String nameDriver, String address, int zip, String city, int phoneNr, String email, int licenseNr, String sinceDate, String comName, String comAddress, int comPhone, int comReg) throws IOException {
+        //anvender localdate til at bruge den aktuelle dato på hvornår kontrakten oprettes
+        LocalDate currentDate = LocalDate.now();
 
+        //Vi vil have systemet til at oprette en fil der hedder Contract + dagens dato
+        String fileName = "Contract_" + currentDate + ".txt";
 
+        //Filen oprettes
+        FileWriter writer = new FileWriter(fileName);
+
+        // Write some content to the file
+        writer.write("This is a new contract created on " + currentDate + "." + "\n");
+
+        writer.write("Name of Driver: " + nameDriver + "\n");
+        writer.write("Address: " + address + "\n");
+        writer.write("Zip code: " + zip + "\n");
+        writer.write("City: "+ city + "\n");
+        writer.write("Phone number: " + phoneNr + "\n");
+        writer.write("E-mail: " + email + "\n");
+        writer.write("Drives License Number: " + licenseNr + "\n");
+        writer.write("Driver since date: " + sinceDate + "\n");
+
+        writer.write("Comany Name: " + comName + "\n");
+        writer.write("Company Address: " + comAddress + "\n");
+        writer.write("Company Phone number: " + comPhone + "\n");
+        writer.write("Company Registration Number: "+ comReg + "\n");
+
+        writer.close();
+    }
     public static void options(Scanner input) {
 
         System.out.println("Select car category:\nPress 1 for Family\nPress 2 for Luxery\nPress 3 for Sport\nPress 0 to quit.");
